d183d875286ca622bdbed4330088f3472075fd1e
SamAdeniji <117260126+SamAdeniji@users.noreply.github.com>
2023-01-07 00:51:20 -0600

    setting up

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("count-element").innerText = 0;
+document.getElementById("myButton").innerHTML;
+let counterDisplay = document.getElementById("count-element").innerText;
+let clickElement = document.getElementById("myButton").innerHTML;
+// alert (counterDisplay);
+clickElement.addEventListener("click", incrementFunc);
+function incrementFunc() {
+    let counterDisplay = 0;
+    let carsCounted = counterDisplay + 1;
+        alert (carsCounted);
+};
