efbfd4bedc8f436fe0f0c3cf414fb431dc3f0169
SamAdeniji <117260126+SamAdeniji@users.noreply.github.com>
2023-01-07 21:00:21 -0600

    adjustment

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.getElementById("count-element").innerText = 0;
+document.getElementById("count-element").innerText = 4;
 document.getElementById("myButton").innerHTML = "Increase";
 let counterDisplay = document.getElementById("count-element").innerText;
 let clickElement = document.getElementById("myButton").innerHTML;
 // alert (counterDisplay);
-clickElement.addEventListener("click", incrementFunc);
-function incrementFunc() {
+clickElement.addEventListener("click", incrementFunc() {
     counterDisplay = 1;
-};
+});
