e6021d47baa3f5f1fd7869c7eb06d9c546ed86d8
SamAdeniji <117260126+SamAdeniji@users.noreply.github.com>
2023-01-08 18:54:38 -0600

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("count-element").innerText;
+document.getElementById("count-element").innerText = 0;
 document.getElementById("myButton").innerHTML;
 document.getElementById("myButton").innerHTML.addEventListener("click", incrementFunc());
 function incrementFunc() {
-    document.getElementById("text-item").innerText
-    document.getElementById("count-element").innerText ++;
+    // document.getElementById("text-item").innerText
+    document.getElementById("count-element").innerText +=;
 };
 
